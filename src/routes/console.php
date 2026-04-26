5e60cd9888a3cecd49ab94953e084ddea2716fb1
Nate Morris <nate.morris@marconet.com>
2026-04-26 12:42:55 -0500

    Remove unnecessary artisan command registration
    Laravel 12 auto-registers commands in app/Console/Commands/.

--- src/routes/console.php
+++ src/routes/console.php
@@ -1,13 +1,8 @@
 <?php
 
-use App\Console\Commands\RedownloadMedia;
-use App\Console\Commands\ResetStuckProcessing;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-
-app()->make(RedownloadMedia::class);
-app()->make(ResetStuckProcessing::class);
